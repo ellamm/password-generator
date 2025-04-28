ad7c33bfef63cf98f00deafc1f4a3b281caad8e2
Mihaela <26812755+ellamm@users.noreply.github.com>
2025-04-28 14:25:05 +0200

    generate passwords

---
+++ index.js
@@ -0,0 +1,117 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+const generateButton = document.getElementById("generate-btn");
+
+generateButton.addEventListener("click", function () {
+  const pass1 = document.getElementById("pass-1-button");
+  const pass2 = document.getElementById("pass-2-button");
+  pass1.textContent = generateRandomPassword(15);
+  pass2.textContent = generateRandomPassword(12);
+});
+
+function getRandomCharacter() {
+  const randomIndex = Math.floor(
+    Math.random(characters.length) * characters.length
+  );
+  return characters[randomIndex];
+}
+
+function generateRandomPassword(passLength) {
+  let password = "";
+  for (let i = 1; i < passLength; i++) {
+    password += getRandomCharacter();
+  }
+  return password;
+}
